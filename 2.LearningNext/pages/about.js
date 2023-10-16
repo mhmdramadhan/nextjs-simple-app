eba1a055cf2e07b9cbb0b0e02dff23fccfde3f9a
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-10-16 14:11:54 +0700

    push data

--- 2.LearningNext/pages/about.js
+++ 2.LearningNext/pages/about.js
@@ -1,12 +1,26 @@
-import Link from "next/link";
-
-
-export default function About() {
+export default function About(props) {
   return (
     <>
-      <h1>About page</h1>
-      <p>laasd DADASDAa asdasd asdasd asdasd a dasd</p>
-      <Link href="/">Back to Home page</Link>
+      <h2>About Us</h2>
+      <p>
+        Welcome to this amazing about page. Lorem ipsum dolor, sit amet
+        consectetur adipisicing elit. Porro dolore officiis atque voluptas,
+        quas, repellendus cum, magnam a alias unde reiciendis voluptates aliquam
+        maxime laborum? Quae omnis eius impedit et?
+      </p>
+      <p>I have {props.repoCount} public repos on GitHub.</p>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('https://api.github.com/users/mhmdramadhan');
+  const data = await response.json();
+
+  return {
+    props: {
+      repoCount: data.public_repos,
+    },
+    revalidate: 10,
+  };
 }
