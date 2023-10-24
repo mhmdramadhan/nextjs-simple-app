bbc7289706d6f7f0bfef25325152d1e6c85b84d5
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-10-24 09:40:48 +0700

    generic function

--- 4.Typescript/basic.ts
+++ 4.Typescript/basic.ts
@@ -38,3 +38,27 @@ let people: Person[];
 let course: string | number = 'React the complete guide';
 
 course = 1234;
+
+// function and Types
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function print(value: any) {
+  console.log(value);
+}
+
+// Generics
+
+function insertAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+// updatedArray[0].split('');
