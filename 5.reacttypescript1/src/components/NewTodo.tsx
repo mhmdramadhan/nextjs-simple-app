1fab3db0e221e78e92b1ad2b7a04b58cf66e8453
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-10-26 15:25:21 +0700

    adding component Todoitem

---
+++ 5.reacttypescript1/src/components/NewTodo.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useRef } from 'react';
+
+const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
