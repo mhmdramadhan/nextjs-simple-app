aefa790c02906dc5882facc340f4b4b81198a486
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-10-27 10:03:48 +0700

    margin state typescript

--- 5.reacttypescript1/src/App.tsx
+++ 5.reacttypescript1/src/App.tsx
@@ -1,15 +1,25 @@
 import Todos from './components/Todos';
 import './App.css';
 import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
+import { useState } from 'react';
 
 function App() {
-  const todos = [
-    new Todo('Learn Reactss'),
-    new Todo('Learn Typescript'),
-  ]
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    // inialisasi todo baru
+    const newTodo = new Todo(todoText);
+
+    // simpan todo baru ke array todos
+    setTodos((prevTodos) => {
+      return [...prevTodos, newTodo];
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
--- 5.reacttypescript1/src/components/NewTodo.tsx
+++ 5.reacttypescript1/src/components/NewTodo.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { useRef } from 'react';
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null);
 
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const enteredText = todoTextInputRef.current
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
