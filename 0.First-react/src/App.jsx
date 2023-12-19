851a068e51ac60050fa3faa1a4b58ef98c943f18
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-12-19 16:03:19 +0700

    push daya

--- 0.First-react/src/App.jsx
+++ 0.First-react/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-slate-900 grid place-content-center min-h-screen">
+      <div className={'flex gap-x-2'}>
+        <Button>Sign In</Button>
+        <Button>Sign Up</Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+function Button({ children }) {
+  return (
+    <button className={'bg-blue-600 text-white px-4 py-2 rounded'}>
+      {children}
+    </button>
+  );
+}
+
+export default App;
