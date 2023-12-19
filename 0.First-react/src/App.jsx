d83c08a64ba8148e81ef7553a5dbacb1174eb751
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2023-12-20 06:22:16 +0700

    push data

--- 0.First-react/src/App.jsx
+++ 0.First-react/src/App.jsx
@@ -1,17 +1,21 @@
 function App() {
   return (
     <div className="bg-slate-900 grid place-content-center min-h-screen">
-      <div className={'flex gap-x-2'}>
-        <Button>Sign In</Button>
-        <Button>Sign Up</Button>
+      <div className={"flex gap-x-2"}>
+        <Button type="submit" className="bg-blue-600">
+          Register
+        </Button>
+        <Button type="button" className="bg-pink-600">
+          Login
+        </Button>
       </div>
     </div>
   );
 }
 
-function Button({ children }) {
+function Button({ children, type, className }) {
   return (
-    <button className={'bg-blue-600 text-white px-4 py-2 rounded'}>
+    <button type={type} className={`${className} text-white px-4 py-2 rounded`}>
       {children}
     </button>
   );
