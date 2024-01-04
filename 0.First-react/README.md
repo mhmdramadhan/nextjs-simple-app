a744727273779992fea62f49d3758d2823df3be9
mhmdramadhan <mr.mhmd.ramadhan@gmail.com>
2024-01-04 10:28:06 +0700

    push data

--- 0.First-react/README.md
+++ 0.First-react/README.md
@@ -1,8 +1,38 @@
-# React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# How To Implement React (simple code)
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+
+## Installation
+
+Install in your local using NPM
+
+```bash
+  npm install
+```
+
+Install in your local using YARN
+
+```bash
+  yarn add
+```
+
+Run APP
+
+```bash
+  npm run dev
+```
+    
+## Documentation
+
+[Documentation React](https://react.dev/)
+
+- component card
+- using state
+
+
+## Screenshots
+
+![App Screenshot](https://raw.githubusercontent.com/mhmdramadhan/nextjs-simple-app/main/0.First-react/task.png)
+
--- 0.First-react/src/components/Todo.jsx
+++ 0.First-react/src/components/Todo.jsx
@@ -7,16 +7,41 @@ const Todo = () => {
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
 
+  const handleAddTask = (e) => {
+    e.preventDefault();
+
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      {
+        id: Math.floor(Math.random() * Date.now()),
+        name: newTask,
+        completed: false,
+      },
+    ]);
+  };
+
   return (
     <Card>
       <Card.Title></Card.Title>
       <Card.Body>
-        <div className="item-center flex gap-x-2">
-          <Input />
-          <Button text="Add task" />
-        </div>
+        <form>
+          <div className="item-center flex gap-x-2">
+            <Input
+              value={newTask}
+              onChange={(e) => setNewTask((x) => e.target.value)}
+            />
+            <Button onClick={handleAddTask} text="Add task" />
+          </div>
+        </form>
+        {tasks.length > 0 && (
+          <ol className="space-y-2">
+            {tasks.map((data, key) => (
+              <li key={key}>{data}</li>
+            ))}
+          </ol>
+        )}
       </Card.Body>
-      <Card.Footer>You have 5 tasks.</Card.Footer>
+      <Card.Footer>You have {tasks.length} tasks.</Card.Footer>
     </Card>
   );
 };
